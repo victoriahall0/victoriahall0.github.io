0d91af6d383e299bcd71dc166c2f7b8d4a9b06ff
Victoria Hall <viha7094@colorado.edu>
2023-12-16 19:11:35 -0700

    js midterm draft

---
+++ js/jsmidterm.js
@@ -0,0 +1,34 @@
+const newBtn = document.querySelector("#button");
+const anotherBtn = document.querySelector("#anotherbutton")
+const numBox = document.querySelector(".number");
+const anewBtn = document.querySelector("#newbutton");
+const anewBox = document.querySelector(".congrats");
+let length = 10;
+
+
+function getRandom(length) {
+    let number = Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
+    return number;
+}
+
+function displayNumber() {
+    let phoneNumber = getRandom(length)
+    numBox.textContent = phoneNumber;
+}
+
+newBtn.addEventListener("click", displayNumber)
+anotherBtn.addEventListener("click", displayNumber)
+
+function displayText() {
+    let congratsMessage = "Thank you for submitting your phone number !";
+    anewBox.textContent = congratsMessage;
+}
+
+anewBtn.addEventListener("click", displayText)
+
+
+
+
+
+
+
